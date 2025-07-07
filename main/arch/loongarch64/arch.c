061cc4b04ef798f38e5293c05a80f28aa4075fcb
wheatfox <wheatfox17@icloud.com>
2025-07-07 11:38:06 +0800

    refactor: update memory clearing logic in arch_clear_memory_regions to use configurable hvisor binary load address and improve clarity

--- main/arch/loongarch64/arch.c
+++ main/arch/loongarch64/arch.c
@@ -17,14 +17,14 @@ static void arch_get_char(char *c) {}
 static void arch_memory_init(void) {}
 static void arch_setup_direct_mapping(void) {}
 static void arch_clear_memory_regions(void) {
-  // Don't clear the hvisor binary load address region (0x90000001f0000000)
-  // as it contains the code we're about to jump to
-  UINTN memset2_st = 0x9000000000000000ULL + 0x1000;
-  UINTN memset2_size = 0x10000;
+  UINTN memset1_st = CONFIG_HVISOR_BIN_LOAD_ADDR;
+  UINTN memset1_size = 0x1000000ULL;
+  UINTN memset2_st = 0x9000000000001000ULL;
+  UINTN memset2_size = 0x10000ULL;
   UINTN memset3_st = 0x9000000000200000ULL;
-  UINTN memset3_size = 0x1000000;
-  
-  // Only clear regions that don't overlap with the hvisor binary
+  UINTN memset3_size = 0x1000000ULL;
+
+  memset2((void *)memset1_st, 0, memset1_size);
   memset2((void *)memset2_st, 0, memset2_size);
   memset2((void *)memset3_st, 0, memset3_size);
 }
